4adb65c61a0fad1290858bd3f237ec650d4c8e0d
Anshu_Slatecode <97159372+Github-Anshu@users.noreply.github.com>
2024-11-09 12:45:45 +0530

    Create User.js

---
+++ User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const domains = [
+  "Web Development",
+  "App Development",
+  "Machine Learning",
+  "JAVA",
+  "C++",
+  "DSA",
+];
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  engineeringYear: {
+    type: Number,
+  },
+  domainOfInterest: {
+    type: String,
+    enum: domains,
+  },
+  topicsCompleted: {
+    type: [String],
+    default: [],
+    validate: {
+      validator: function (array) {
+        return array.length === new Set(array).size;
+      },
+      message: "topicsCompleted should contain unique values.",
+    },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
